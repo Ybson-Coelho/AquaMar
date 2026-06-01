ca4d58ed1f2ed9751e92cc53d56dc85d8c0e8e94
Ybson <96489851+Ybson-Coelho@users.noreply.github.com>
2026-06-01 11:50:28 -0300

    chore: atualiza o serviço de arquivos estáticos no server.js
    Refactor static file serving to use dynamic public path.

--- src/server.js
+++ src/server.js
@@ -2,18 +2,19 @@ import express from "express";
 import path from "path";
 
 const app = express();
-const port = 3000;
 
-app.use(express.static("src/public"));
+const publicPath = path.resolve(process.cwd(), "src/public");
+
+app.use(express.static(publicPath));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve("src/public/pages/index.html"));
+  res.sendFile(path.join(publicPath, "pages/index.html"));
 });
 
 app.get("/:page", (req, res) => {
   const { page } = req.params;
 
-  res.sendFile(path.resolve(`src/public/pages/${page}.html`), (err) => {
+  res.sendFile(path.join(publicPath, `pages/${page}.html`), (err) => {
     if (err) {
       res.redirect("/");
     }
